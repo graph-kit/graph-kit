409557a6eef02fe18c52d2f5c144c600bc790a43
teykamp <thomas@eykamp.com>
2026-08-11 23:00:00 -0400

    clean up action bar

--- packages/magic-products/src/min-spanning-trees/ActionBar.vue
+++ packages/magic-products/src/min-spanning-trees/ActionBar.vue
@@ -1,78 +1,48 @@
 <script setup lang="ts">
   import Button from '@magic/shared/Button';
   import HStackVue from '@magic/shared/HStack';
-  import Well from '@magic/shared/Well';
   import { useProvidedMagicGraph } from '@magic/shared/product';
   import { useFocusedNode } from '@magic/shared/utilities';
 
-  import { ref, watch } from 'vue';
-
-  import { useKruskalsSimulation, usePrimsSimulation } from './simulations/index.ts';
+  import {
+    useKruskalsSimulation,
+    usePrimsSimulation,
+  } from './simulations/index.ts';
 
   const graph = useProvidedMagicGraph();
 
   const prims = usePrimsSimulation();
   const kruskals = useKruskalsSimulation();
 
-  const node = useFocusedNode(graph);
-
-  const awaitingPrimsStartNode = ref(false);
-
-  const armPrims = () => {
-    graph.focus.clear();
-    awaitingPrimsStartNode.value = true;
-  };
-
-  const cancelPrims = () => {
-    awaitingPrimsStartNode.value = false;
-  };
+  const focusedNode = useFocusedNode(graph);
 
   const startKruskals = () => {
     graph.magic.simulation.start(kruskals.kruskals);
     graph.focus.clear();
   };
 
-  watch(node, (focusedNode) => {
-    if (!awaitingPrimsStartNode.value || !focusedNode) return;
-    prims.startNodeId.value = focusedNode.id;
+  const startPrims = () => {
+    if (!focusedNode.value) return;
+    prims.startNodeId.value = focusedNode.value.id;
     graph.magic.simulation.start(prims.prims);
     graph.focus.clear();
-    awaitingPrimsStartNode.value = false;
-  });
+  };
 </script>
 
 <template>
-  <div v-if="!graph.magic.simulation.current.value">
-    <HStackVue
-      v-if="!awaitingPrimsStartNode"
-      class="p-1"
+  <HStackVue v-if="!graph.magic.simulation.current.value">
+    <Button
+      @click="startPrims"
+      class="text-lg"
+      :disabled="!focusedNode"
     >
-      <Button
-        @click="armPrims"
-        class="text-lg"
-      >
-        Prim's
-      </Button>
-      <Button
-        @click="startKruskals"
-        class="text-lg"
-      >
-        Kruskal's
-      </Button>
-    </HStackVue>
-    <HStackVue
-      v-else
-      class="p-1 items-center"
+      Prim's
+    </Button>
+    <Button
+      @click="startKruskals"
+      class="text-lg"
     >
-      <Well class="font-bold text-xl">
-        Click a Node to Start Prim's From!
-      </Well>
-      <Button
-        @click="cancelPrims"
-        class="text-lg"
-      >
-        Cancel
-      </Button>
-    </HStackVue>
-  </div>
+      Kruskal's
+    </Button>
+  </HStackVue>
 </template>

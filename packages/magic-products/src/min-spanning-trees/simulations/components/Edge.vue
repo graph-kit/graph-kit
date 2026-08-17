5dd3b1f26cc5ba2fb2983cc6cb843d9eb536492c
teykamp <thomas@eykamp.com>
2026-08-17 00:32:36 -0400

    add edge component

---
+++ packages/magic-products/src/min-spanning-trees/simulations/components/Edge.vue
@@ -0,0 +1,55 @@
+<script setup lang="ts">
+  import { useProvidedGraph } from '@magic/shared/graph-product';
+  import type { GEdge } from '@magic/shared/graph/types';
+  import { useEdgeStyles } from '@magic/shared/theme';
+
+  import { StyleValue, computed, onUnmounted } from 'vue';
+
+  const props = withDefaults(
+    defineProps<{
+      id: GEdge['id'];
+      scale?: number;
+    }>(),
+    { scale: 1 },
+  );
+
+  const graph = useProvidedGraph();
+
+  const { styles, dispose } = useEdgeStyles(graph, props.id);
+
+  const edgeStyle = computed<StyleValue>(() => ({
+    backgroundColor: styles.value.color,
+    height: styles.value.width * props.scale + 'px',
+    placeItems: 'center',
+  }));
+
+  const labelStyle = computed<StyleValue>(() => ({
+    fontSize: `calc(1rem * ${props.scale})`,
+  }));
+
+  onUnmounted(dispose);
+</script>
+
+<template>
+  <div
+    @click="graph.focus.set([id])"
+    class="flex items-center cursor-pointer"
+  >
+    <div
+      class="flex-1 h-full"
+      :style="edgeStyle"
+    />
+
+    <span
+      class="px-2 font-bold whitespace-nowrap"
+      :style="labelStyle"
+    >
+      {{ styles.text.content }}
+    </span>
+
+    <div
+      class="flex-1 h-full"
+      :style="edgeStyle"
+    />
+  </div>
+</template>

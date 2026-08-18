1f71e3ed22013be352a5c0744d31d719430e7e46
yonava <yonavossandreae@gmail.com>
2026-08-17 19:47:07 -0700

    polish deets

--- packages/magic-shared/src/shortcuts/useProductShortcuts.ts
+++ packages/magic-shared/src/shortcuts/useProductShortcuts.ts
@@ -6,36 +6,48 @@ import { ShortcutItem } from './useShortcuts.ts';
 export const useProductShortcuts = (magic: Magic) => {
   const fullscreen = useFullscreen();
   // TODO make it windows + mac agnostic
-  const shortcuts: ShortcutItem[] = [
+  const shortcuts: (ShortcutItem | undefined)[] = [
     {
       id: 'product/fullscreen',
       key: 'f',
       callback: fullscreen.toggle,
     },
+    magic.annotations
+      ? {
+          id: 'product/toggle-annotations',
+          key: 'a',
+          callback: magic.annotations.toggle,
+        }
+      : undefined,
     {
       id: 'product/toggle-component-slot-ui',
       key: 'meta+.',
       callback: magic.componentSlots.visibility.toggle,
     },
-    {
-      id: 'product/undo',
-      key: 'meta+z',
-      callback: () => {
-        if (!magic.history?.canUndo.value) return;
-        magic.history.undo();
-      },
-    },
-    {
-      id: 'product/redo',
-      key: 'meta+shift+z',
-      callback: () => {
-        if (!magic.history?.canRedo.value) return;
-        magic.history.redo();
-      },
-    },
+    magic.history
+      ? {
+          id: 'product/undo',
+          key: 'meta+z',
+          callback: () => {
+            if (!magic.history?.canUndo.value) return;
+            magic.history.undo();
+          },
+        }
+      : undefined,
+    magic.history
+      ? {
+          id: 'product/redo',
+          key: 'meta+shift+z',
+          callback: () => {
+            if (!magic.history?.canRedo.value) return;
+            magic.history.redo();
+          },
+        }
+      : undefined,
   ];
 
   for (const shortcut of shortcuts) {
+    if (!shortcut) continue;
     magic.shortcuts.add(shortcut);
   }
 };

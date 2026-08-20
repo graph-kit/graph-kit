e6be48e05a08880340560753b6ec7f66b8c6b6d7
yonava <yonavossandreae@gmail.com>
2026-08-19 21:14:38 -0400

    dead local storage code removal

--- packages/core-utils/src/localStorage.ts
+++ packages/core-utils/src/localStorage.ts
@@ -1,53 +1,5 @@
 import { devWarning } from './debugging.ts';
 
-type LocalStorageGetter = (...args: any[]) => string;
-type LocalStorageRecord = Record<string, string | LocalStorageGetter>;
-
-/**
- * a registry for all localStorage keys this application uses
- */
-export const localKeys = {
-  /** nodes in graph product */
-  nodes: (key: string) => `nodes-${key}` as const,
-  /** edges in graph product */
-  edges: (key: string) => `edges-${key}` as const,
-  /** graph product simulation speed */
-  simulationPlaybackSpeed: 'simulation-playback-speed',
-  /** graph theme set by user - {@link Graph.preferredTheme} */
-  preferredTheme: 'preferred-theme',
-} as const satisfies LocalStorageRecord;
-
-/**
- * all return values of localStorage are, by default, string.
- * this type allows string to be narrowed to types such as 'true' | 'false'
- */
-type TypeOverride = {};
-
-type LocalObj = typeof localKeys;
-
-/**
- * @example
- * type T = TypeOrReturnType<number> // number
- * type TFunc = TypeOrReturnType<() => number> // number
- */
-type TypeOrReturnType<T> = T extends (...args: any[]) => infer U ? U : T;
-
-type LocalKeys = TypeOrReturnType<LocalObj[keyof LocalObj]>;
-type LocalType<T extends LocalKeys> = T extends keyof TypeOverride
-  ? TypeOverride[T]
-  : string;
-
-/**
- * perform **type safe** localStorage actions
- */
-export const local = {
-  get: <T extends LocalKeys>(key: T) => localStorage.getItem(key),
-  set: <T extends LocalKeys, K extends LocalType<T>>(key: T, value: K) =>
-    localStorage.setItem(key, value),
-  remove: <T extends LocalKeys>(key: T) => localStorage.removeItem(key),
-  clear: () => localStorage.clear(),
-};
-
 let store: Storage | null | undefined;
 
 /**

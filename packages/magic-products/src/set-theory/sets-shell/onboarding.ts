5a03c41f83d9fe95cb23ef4505fb6ce45adad199
Yona Alexander Voss-Andreae <yonavossandreae@gmail.com>
2026-09-07 20:54:16 -0700

    fix(product/onboarding): add backspace to onboarding (#1073)

--- packages/magic-products/src/set-theory/sets-shell/onboarding.ts
+++ packages/magic-products/src/set-theory/sets-shell/onboarding.ts
@@ -2,13 +2,17 @@ import {
   OnboardingItem,
   SHARED_ONBOARDING_ITEMS,
 } from '@magic/shared/onboarding';
-import { mdiGestureDoubleTap } from '@mdi/js';
+import { mdiBackspace, mdiGestureDoubleTap } from '@mdi/js';
 
 /** what sets suggests trying first, shown over a canvas with no sets on it */
 export const SETS_ONBOARDING: OnboardingItem[] = [
   {
     icon: mdiGestureDoubleTap,
-    display: 'Double click to create a set',
+    display: 'Double click to create set',
+  },
+  {
+    icon: mdiBackspace,
+    display: 'Backspace to remove set',
   },
   ...SHARED_ONBOARDING_ITEMS,
 ];
